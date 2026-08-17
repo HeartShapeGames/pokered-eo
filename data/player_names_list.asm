49914a7d7a75c2040bcb4a8bc0c40c6ae60c1124
HeartShapeGames <49497286+HeartShapeGames@users.noreply.github.com>
2026-08-17 20:43:27 +0200

    Update player_names_list.asm

--- data/player_names_list.asm
+++ data/player_names_list.asm
@@ -1,27 +1,27 @@
 IF DEF(_RED)
 DefaultNamesPlayerList:
 	db "NOVA NOMO@"
-	db "RED@"
-	db "ASH@"
-	db "JACK@"
+	db "RUĜO@"
+	db "AŜO@"
+	db "JAKO@"
 
 DefaultNamesRivalList:
 	db "NOVA NOMO@"
-	db "BLUE@"
-	db "GARY@"
-	db "JOHN@"
+	db "BLUO@"
+	db "GARJO@"
+	db "JONO@"
 ENDC
 
 IF DEF(_BLUE)
 DefaultNamesPlayerList:
 	db "NOVA NOMO@"
-	db "BLUE@"
-	db "GARY@"
-	db "JOHN@"
+	db "BLUO@"
+	db "GARJO@"
+	db "JONO@"
 
 DefaultNamesRivalList:
 	db "NOVA NOMO@"
-	db "RED@"
-	db "ASH@"
-	db "JACK@"
+	db "RUĜO@"
+	db "AŜO@"
+	db "JAKO@"
 ENDC
